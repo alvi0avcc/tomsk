0bd304c2d4f678954c49dc770b47e9ae4f2eaf2d
Aleksandr <127627433+alvi0avcc@users.noreply.github.com>
2024-10-11 23:22:36 +0300

    avcc-carousel fin

--- new_avcc/js/main.js
+++ new_avcc/js/main.js
@@ -3,13 +3,18 @@ const leftMenuButton = document.getElementById('left-menu-button');
 leftMenuButton.addEventListener('click', ()=>{
     const leftMenu = document.getElementById('aside-left');
     leftMenu.classList.toggle('side-menu-show');
-
+    const leftMenuBtn = document.getElementById('left-menu-button');
+    leftMenuBtn.classList.toggle('left-menu-button-pos');
+    leftMenuBtn.classList.toggle('left-menu-button-pos::before');
 });
 
 const rightMenuButton = document.getElementById('right-menu-button');
 rightMenuButton.addEventListener('click', ()=>{
     const rightMenu = document.getElementById('aside-right');
     rightMenu.classList.toggle('side-menu-show');
+    const rightMenuBtn = document.getElementById('right-menu-button');
+    rightMenuBtn.classList.toggle('right-menu-button-pos');
+    rightMenuBtn.classList.toggle('right-menu-button-pos::before');
 
 });
 
@@ -19,10 +24,23 @@ rightMenuButton.addEventListener('click', ()=>{
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     // direction: 'vertical',
-    slidesPerView: 3,
+    slidesPerView: 1,
     loop: true,
     spaceBetween: 5,
 
+    breakpoints: {
+      // when window width is >= 1500px
+      1500: {
+        slidesPerView: 4,
+      },
+      // when window width is >= 1000px
+      1000: {
+        slidesPerView: 3,
+      },
+      800: {
+        slidesPerView: 2,
+      },
+    },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
